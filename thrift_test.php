bc4af000903b534ffc09f624028210c9a0aaa4b4
zhaokai021 <zhaokai021@yeah.net>
2014-04-16 10:48:47 +0800

    update

--- thrift_test.php
+++ thrift_test.php
@@ -3,7 +3,7 @@
 ini_set('display_errors', E_ALL);  
 
 require_once( './ThriftPHP.php' ); 
-$cli = new ThriftPHP('61.4.185.193','9090','','');
+$cli = new ThriftPHP('127.0.0.1','9090','','');
 print_r($cli->getAllTableNames());
 print_r($cli->query("dict_station",array("101010100"),array("station")));
 $cli->insert("hbasephp",array(
